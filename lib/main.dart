61ee84178a006231fb275711516e1171a96b2be2
anna shen <annashens.can@gmail.com>
2025-03-02 15:24:30 -0500

    remove redundnat import in main

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:illuden/widgets/bluetooth/cubit/bluetooth_cubit.dart';
 import 'package:equatable/equatable.dart';
 
