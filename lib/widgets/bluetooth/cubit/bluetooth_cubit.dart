b7136f546606e0f3617efb7c8fca7e7cfc80e854
Stanley Hao <hao.stanley@yahoo.ca>
2025-03-02 21:08:54 -0500

    Implement single and multi-write for bluetooth cubit write function

--- lib/widgets/bluetooth/cubit/bluetooth_cubit.dart
+++ lib/widgets/bluetooth/cubit/bluetooth_cubit.dart
@@ -12,16 +12,23 @@ class BluetoothState {
   final BluetoothAdapterState adapterState;
   final BluetoothConnectionState connectionState;
   final BluetoothDevice? device;
-  BluetoothState({this.isScanning = false, this.isConnected = false, this.device, this.adapterState = BluetoothAdapterState.unknown, this.connectionState = BluetoothConnectionState.disconnected});
+  BluetoothState(
+      {this.isScanning = false,
+      this.isConnected = false,
+      this.device,
+      this.adapterState = BluetoothAdapterState.unknown,
+      this.connectionState = BluetoothConnectionState.disconnected});
 }
 
 // Bluetooth Cubit for handling bluetooth connection, read and writing
 class BluetoothCubit extends Cubit<BluetoothState> {
-  final String targetDeviceName = "HUNTER2_DEMO"; // Change this to your device's name
+  final String targetDeviceName =
+      "HUNTER2_DEMO"; // Change this to your device's name
   late StreamSubscription<BluetoothAdapterState> _deviceStateSubscription;
-  late StreamSubscription<BluetoothConnectionState> _connectionStateSubscription;
+  late StreamSubscription<BluetoothConnectionState>
+      _connectionStateSubscription;
 
-  // Bluetooth services 
+  // Bluetooth services
   late List<BluetoothService>? _services;
   late BluetoothService? _rwService;
   late BluetoothCharacteristic? _readCharacteristic;
@@ -46,7 +53,8 @@ class BluetoothCubit extends Cubit<BluetoothState> {
       await FlutterBluePlus.turnOn();
     }
 
-    _deviceStateSubscription = FlutterBluePlus.adapterState.listen((BluetoothAdapterState state) {
+    _deviceStateSubscription =
+        FlutterBluePlus.adapterState.listen((BluetoothAdapterState state) {
       emit(BluetoothState(adapterState: state));
       if (state == BluetoothAdapterState.on) {
         _startScan();
@@ -58,22 +66,25 @@ class BluetoothCubit extends Cubit<BluetoothState> {
   void _startScan() async {
     try {
       await FlutterBluePlus.startScan(
-        withServices:[Guid("180D")],
-        withNames:[targetDeviceName],
+        withServices: [Guid("180D")],
+        withNames: [targetDeviceName],
         // timeout: const Duration(seconds: 15)
       );
-      emit(BluetoothState(isScanning: true));   
+      emit(BluetoothState(isScanning: true));
     } catch (e) {
-      Snackbar.show(ABC.b, prettyException("Start Scan Error:", e), success: false);
+      Snackbar.show(ABC.b, prettyException("Start Scan Error:", e),
+          success: false);
       print(e);
       _checkAndConnect();
     }
 
-    var subscription = FlutterBluePlus.onScanResults.listen((results) {
-        if (results.isNotEmpty) {  
+    var subscription = FlutterBluePlus.onScanResults.listen(
+      (results) {
+        if (results.isNotEmpty) {
           for (ScanResult result in results) {
             if (result.device.advName == targetDeviceName) {
-              print("Found device: ${result.device.advName} with remote ID: ${result.device.remoteId}");
+              print(
+                  "Found device: ${result.device.advName} with remote ID: ${result.device.remoteId}");
               emit(BluetoothState(device: result.device));
               _connectToDevice(result.device);
             }
@@ -90,7 +101,8 @@ class BluetoothCubit extends Cubit<BluetoothState> {
   Future<void> _connectToDevice(BluetoothDevice device) async {
     try {
       await device.connect();
-      _connectionStateSubscription = device.connectionState.listen((state) async {
+      _connectionStateSubscription =
+          device.connectionState.listen((state) async {
         if (state == BluetoothConnectionState.disconnected) {
           print("Device disconnected. Re-scanning...");
           emit(BluetoothState(isConnected: false));
@@ -100,7 +112,8 @@ class BluetoothCubit extends Cubit<BluetoothState> {
         } else if (state == BluetoothConnectionState.connected) {
           print("Device connected.");
           FlutterBluePlus.stopScan();
-          emit(BluetoothState(isConnected: true, device: device, isScanning: false));
+          emit(BluetoothState(
+              isConnected: true, device: device, isScanning: false));
 
           // Poll for bluetooth services
           _services = await device.discoverServices();
@@ -108,18 +121,20 @@ class BluetoothCubit extends Cubit<BluetoothState> {
             print("Service: ${service.uuid}");
             if (service.uuid == Guid("00ff")) {
               _rwService = service;
-              _readCharacteristic = service.characteristics.firstWhere((c) => c.uuid == Guid("FF01"));
-              _writeCharacteristic = service.characteristics.firstWhere((c) => c.uuid == Guid("FF02"));
+              _readCharacteristic = service.characteristics
+                  .firstWhere((c) => c.uuid == Guid("FF01"));
+              _writeCharacteristic = service.characteristics
+                  .firstWhere((c) => c.uuid == Guid("FF02"));
               return;
             }
           }
           print("Missing service 0x00FF!!!!");
         }
       });
-      
     } catch (e) {
       emit(BluetoothState(isConnected: false));
-      print("Connection to $targetDeviceName failed with error $e. Re-scanning...");
+      print(
+          "Connection to $targetDeviceName failed with error $e. Re-scanning...");
       Future.delayed(const Duration(seconds: 5), () {
         _startScan();
       });
@@ -135,16 +150,37 @@ class BluetoothCubit extends Cubit<BluetoothState> {
       emit(BluetoothState(isConnected: false));
     }
   }
-  
-  // Write to the device
-  void write() async{
+
+  // Write to the device with one of the following types:
+  // 1: Single write
+  // 2: Multiple write (not implemented)
+  // 3: Identical write
+  void write(int type, List<int> addresses,
+      [int w2700 = 0,
+      int w5000 = 0,
+      int w6500 = 0,
+      int r = 0,
+      int g = 0,
+      int b = 0]) async {
     print("Trying to write to device...");
     if (state.isConnected && _writeCharacteristic != null) {
-      await _writeCharacteristic!.write([0x1, 0x2, 0x3, 0x4, 0x5, 0x6]);
-      print("Data written to device.");
+      if (type == 3) {
+
+        // Identical write
+        List<int> msg = [type, w2700, w5000, w6500, r, g, b];
+        msg.addAll(addresses);\
+        await _writeCharacteristic!.write(msg);
+        print("Identical Write Completed");
+      } else if (type == 1) {
+
+        // Single write
+        await _writeCharacteristic!.write([type, addresses[0], w2700, w5000, w6500, r, g, b]);
+        print("Single Write Completed");
+      } else {
+        print("Invalid write type.");
+      }
     } else {
       print("Device not connected or write characteristic not found.");
     }
-    // emit(BluetoothState());
   }
 }
